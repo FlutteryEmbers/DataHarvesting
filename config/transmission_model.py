bf1fce93e74284a9c830a6ef39f8dd98e010178e
JerryZyc <43042078+JerryZyc@users.noreply.github.com>
2022-05-06 22:08:43 -0400

    Update transmission_model.py

--- config/transmission_model.py
+++ config/transmission_model.py
@@ -10,7 +10,7 @@ Phi_list=np.array([5,4,3])
 
 def Phi_dif_transmitting_speed(agent_position,X_target_position,data_volume_required,data_volume_collected):
 
-     n_target_number=np.length(data_to_collect_list)
+     n_target_number=np.length(data_volume_collected)
   
      data_transmitting_rate_list=np.zeros((1, n_target_number))
      distance=np.zeros((1, n_target_number))
@@ -18,13 +18,11 @@ def Phi_dif_transmitting_speed(agent_position,X_target_position,data_volume_requ
      for i in range(0,n_target_number):
           distance[i] = abs(agent_position-X_target_position[i])
 
-          if data_collected_list[i]>= data_to_collect_list[i]:
+          if data_volume_collected[i]>= data_volume_required[i]:
                data_transmitting_rate_list[i] = 0
           else:
                data_transmitting_rate_list[i] = Phi_list[i]*B*math.log2(1+K/(N*(distance[i]^2 + height^2)))
 
-     data_collected_list[i] = data_collected_list[i] + data_transmitting_rate_list[i]*time_ratio
-     data_to_collect_list[i] = data_collected_list[i] - data_transmitting_rate_list[i]*time_ratio
-
+     data_volume_collected[i] = data_volume_collected[i] + data_transmitting_rate_list[i]*time_ratio
   
-     return data_to_collect_list,data_collected_list,data_transmitting_rate_list
+     return data_volume_collected,data_transmitting_rate_list
