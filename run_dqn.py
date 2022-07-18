52ef8b983b923d3d6c63dbdaf7be3bf1a8acb3c3
shili.wu <shili.wu@hotmail.com>
2022-07-18 08:03:29 -0500

    timer & ignore results

--- run_dqn.py
+++ run_dqn.py
@@ -1,12 +1,14 @@
 from trainer.Q_Learning.ddqn import DDQN, MEMORY_CAPACITY
 from environments.instances import single_diescreteV1
 from utils.utils import plot_curve
+from utils.utils import Timer
                    
 
 n_games = 500
 best_num_steps = 9999999999999999
 episode_rewards = []
 num_steps = []
+timer = Timer()
 
 env = single_diescreteV1.Test_Environment
 ddqn = DDQN(inputs=len(env.status_tracker.get_state()), outputs=env.action_space.n, env=env)
@@ -16,6 +18,7 @@ for i in range(n_games):
     s, current_position = env.reset()
     episode_reward_sum = 0
 
+    timer.start()
     while True:
         # env.render()
         a = ddqn.choose_action(s, current_position)
@@ -33,13 +36,14 @@ for i in range(n_games):
             print('episode%s---reward_sum: %s' % (i, round(episode_reward_sum, 2)))
             env.view()
             break
-
+        
     num_steps.append(env.num_steps)
     if env.num_steps < best_num_steps:
         best_num_steps = env.num_steps
         ddqn.save_models()
 
     episode_rewards.append(round(episode_reward_sum, 2))
+    timer.stop()
         
 x = [i+1 for i in range(n_games)]
 plot_curve(x, episode_rewards, './results/rewards.png', 1)
